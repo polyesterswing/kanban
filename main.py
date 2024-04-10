a8afe1bde2505e33281da00171b990faf5866d94
polyesterswing <kurianjojo2004@gmail.com>
2024-04-10 14:01:44 +0530

    Use textual widgets

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
 from textual.app import App, ComposeResult
-from textual.widget import Widget
-from textual.reactive import Reactive
-from rich.console import RenderableType
-from rich.padding import Padding
-from rich.align import Align
-from rich.text import Text
-from rich.table import Table, box
-from rich.panel import Panel
-from textual.widgets import Footer, Label, ListItem, ListView
+from textual.widgets import Static, ListView, ListItem, Label, Header
+from textual.containers import VerticalScroll, HorizontalScroll, Horizontal, Vertical
 
-class Card(Widget):
-    def compose(self) -> ComposeResult:
-        yield
+from textual.screen import Screen
 
-class Grid(Widget):
-    table = Table(title="Jobs", expand=True, show_lines=False, box=box.SIMPLE_HEAD)
-
-    table.add_column("Todo", ratio=1)
-    table.add_column("Doing", ratio=1)
-    table.add_column("Done", ratio=1)
-
-    table1 = Table.grid(expand = True)
-    table2 = Table.grid(expand = True)
-    table3 = Table.grid(expand = True)
-
-    table1.add_row(
-        Panel("Find out why the component doesnt work?", border_style="blue"),
-    )
-    table1.add_row(
-        Panel("Fix the Component", border_style="blue"),
-    )
-
-    table2.add_row(
-        Panel("ABC", border_style="green"),
-    )
-
-    table3.add_row(
-        Panel("ABC", border_style="red"),
-    )
-
-
-    table.add_row(
-            table1,
-            table2,
-            table3,
-    )
-
-    def render(self) -> RenderableType:
-        return self.table
-
-class MainApp(App):
+class Column(VerticalScroll):
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
 
     def compose(self) -> ComposeResult:
-        yield Grid()
+        for i in range(3):
+            yield Label("abc", classes=f"card col{self.color}")
 
-MainApp().run()
+class Kanban(App):
+    CSS_PATH = "main.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Label("Kanban Board", id="heading")
+        with HorizontalScroll():
+            with VerticalScroll():
+                yield Label("Todo", classes="status")
+                yield Column(1)
+            with VerticalScroll():
+                yield Label("In Progress", classes="status")
+                yield Column(2)
+            with VerticalScroll():
+                yield Label("Done", classes="status")
+                yield Column(3)
+
+
+if __name__ == "__main__":
+    app = Kanban()
+    app.run()
