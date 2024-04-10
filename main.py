ccb0797887a28b9a291b03805e1d5b1e351e92b7
polyesterswing <kurianjojo2004@gmail.com>
2024-04-10 18:58:30 +0530

    Add json support

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from textual.app import App, ComposeResult
 from textual.widgets import Static, ListView, ListItem, Label, Header
 from textual.containers import VerticalScroll, HorizontalScroll, Horizontal, Vertical
 from textual.reactive import reactive
+from textual.message import Message
+from textual.css.query import NoMatches
 
 from textual.screen import Screen
 
+import json
+
 class Card(Label):
     selected = reactive(False)
+    text = reactive("")
 
-    def on_mouse_down(self, event) -> None:
-        self.selected = not self.selected
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
 
     def watch_selected(self, selected_value):
         if selected_value:
@@ -17,7 +23,15 @@ class Card(Label):
         else:
             self.remove_class("selected")
 
+    def on_mouse_down(self, event) -> None:
+        self.selected = True
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.text)
+
 class Column(VerticalScroll):
+    cards = reactive([], recompose=True)
+
     def __init__(self, color, cards):
         super().__init__()
         self.color = color
@@ -25,24 +39,30 @@ class Column(VerticalScroll):
 
     def compose(self) -> ComposeResult:
         for card in self.cards:
-            yield Card(card, classes=f"card col{self.color}")
+            c = Card(card)
+            c.add_class("card", f"col{self.color}")
+            yield c
+
+    def on_mouse_up(self, event) -> None:
+        try:
+            card = self.app.query_one(".selected")
+            card.selected = False
+        except NoMatches:
+            pass
 
 class Kanban(App):
     CSS_PATH = "main.tcss"
 
-    def compose(self) -> ComposeResult:
-        yield Label("Kanban Board", id="heading")
-        with HorizontalScroll():
-            with VerticalScroll():
-                yield Label("Todo", classes="status")
-                yield Column(1, ["abc", "def", "ghi"])
-            with VerticalScroll():
-                yield Label("In Progress", classes="status")
-                yield Column(2, ["adjkfjdafkdj adkjsafkjfkajskdajskdjfkasjdfkjsakfjdkjfkdjskfdj", "akdjfae", "adskurakj"])
-            with VerticalScroll():
-                yield Label("Done", classes="status")
-                yield Column(3, ["djakfjd", "akdjf"])
+    f = open("main.json")
+    data = json.load(f)
 
+    def compose(self) -> ComposeResult:
+        yield Label(self.data["heading"], id="heading")
+        with HorizontalScroll():
+                for col, status in enumerate(self.data["statuses"]):
+                    with VerticalScroll():
+                        yield Label(status["title"], classes="status")
+                        yield Column(col % 3, status["cards"])
 
 if __name__ == "__main__":
     app = Kanban()
